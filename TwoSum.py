2742b7b2831eb141102485c3242e9bfebede6fa0
Bartosz Gawin <bartex.gw@gmail.com>
2022-12-15 19:22:19 +0000

    TwoSum not finished

---
+++ TwoSum.py
@@ -0,0 +1,22 @@
+A = [2,7,11,15,17,20,21,-10,-7,-5,-3,-1,0]
+B = 9
+
+#def partition(start,end,numssorted,target):
+
+
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        numsSorted = sorted(nums)
+        return numsSorted
+
+
+
+
+S = Solution()
+print(S.twoSum(A,9))
+
+
+print(5//2)
+
