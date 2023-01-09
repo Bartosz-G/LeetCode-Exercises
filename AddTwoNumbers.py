f7d02f52024df897486b7b68c7e6f9f950c8d283
Bartosz Gawin <bartex.gw@gmail.com>
2023-01-09 18:31:47 +0000

    AddTwoNumbers submitted*

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -1,6 +1,10 @@
 #https://leetcode.com/problems/add-two-numbers/
 
 
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
