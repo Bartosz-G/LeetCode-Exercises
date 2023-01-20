e5521d74a2c4f795d550b28168366b6fe6a4a01e
Bartosz Gawin <bartex.gw@gmail.com>
2023-01-20 13:34:16 +0000

    DuplicateEmails.sql submitted improved

--- DuplicateEmails.sql
+++ DuplicateEmails.sql
@@ -1,8 +1,3 @@
 #https://leetcode.com/problems/duplicate-emails/submissions/881824494/
 
-SELECT DISTINCT Person.email FROM (
-    SELECT count(id),email FROM Person
-        GROUP BY email
-        HAVING count(id)>1)
-    AS TB1,Person
-    WHERE TB1.email = Person.email;
+SELECT email FROM Person GROUP BY email HAVING COUNT(email)>1;
