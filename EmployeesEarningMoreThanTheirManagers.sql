70f7f04b754fd0f914393aae89207d04a74aeab5
Bartosz Gawin <bartex.gw@gmail.com>
2023-01-20 13:26:05 +0000

    DuplicateEmails.sql submitted

--- EmployeesEarningMoreThanTheirManagers.sql
+++ EmployeesEarningMoreThanTheirManagers.sql
@@ -1,8 +1,8 @@
 #https://leetcode.com/problems/employees-earning-more-than-their-managers/
 
-select name as Employee from
-(select t1.name, t1.salary as s1,t2.salary as s2 from employee t1, employee t2
-where t1.managerId = t2.id
-having t1.salary > t2.salary) as DT1;
+select name AS Employee FROM
+(SELECT t1.name, t1.salary as s1,t2.salary as s2 FROM employee t1, employee t2
+WHERE t1.managerId = t2.id
+HAVING t1.salary > t2.salary) AS DT1;
 
 
