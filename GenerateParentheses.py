e8dc1bb6da4b32d0e38d6dd86013794d14541ff2
Bartosz Gawin <bartex.gw@gmail.com>
2023-01-26 18:55:46 +0000

    GenerateParentheses submitted, to be improved

--- GenerateParentheses.py
+++ GenerateParentheses.py
@@ -1,64 +1,38 @@
 # https://leetcode.com/problems/generate-parentheses/
 
 
-a1 = 3
-a2 = ["((()))","(()())","(())()","()(())","()()()"]
 
-b1 = 1
-b2 = ["()"]
+class Solution:
+    def generateParenthesis(self, n: int) -> list[str]:
 
+        List = ['*' * 2 * n]
 
-# class Solution:
-#     def generateParenthesis(self, n: int) -> list[str]:
+        def _generateParenthesis(n,List):
+            finish_condition = 0
 
-def _generateParenthesis(List,l,r,n):
-    if n == 0:
-        return List
+            List_Out = []
 
+            for element in List:
+                left = 0
+                for i, char in enumerate(element):
+                    if char == '*':
+                        if left < 2 * n - i:
+                            List_Out.append(element[:i] + '(' + element[i + 1:])
+                        if left > 0:
+                            List_Out.append(element[:i] + ')' + element[i + 1:])
+                        break
+                    elif char == '(':
+                        left += 1
+                    else:
+                        left -= 1
+                else:
+                    finish_condition += 1
 
+            if finish_condition == len(List):
+                return List
 
+            List_Out
+            return _generateParenthesis(n,List_Out)
 
-
-def addParenthesis(s,l,n):
-    List = []
-    ln = []
-    if l<n:
-        List.append(s + "(")
-        ln.append((l+1,n))
-
-    if l > 0:
-        List.append(s + ")")
-        ln.append((l-1,n-1))
-
-    if not List:
-        return None
-
-    return List,ln
-
-def take_out_elements(List:list):
-
-
-
-# n = 3,l=2
-t1 = "(("
-
-# n = 3, l=0
-t2 = ""
-
-print(addParenthesis(t1,2,3))
-print(addParenthesis('(((',3,3))
-print(addParenthesis('((()',2,2))
-print(addParenthesis('((())',1,1))
-print(addParenthesis('((()))',0,0))
-print(addParenthesis(t2,0,3))
-
-
-
-
-
-
-
-
-
-
+        return _generateParenthesis(n=n,List=List)
 
