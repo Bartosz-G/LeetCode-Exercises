424ed43e7cae86dc86710e8839530a85d7c75987
unknown <bartex.gw@gmail.com>
2023-02-01 21:08:03 +0000

    sqrt() submitted

--- sqrt(x).py
+++ sqrt(x).py
@@ -1,35 +1,18 @@
 #https://leetcode.com/problems/sqrtx/
 
-
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x == 1:
-            return 1
-        if x<1:
+        if x < 1:
             return 0
 
-        def Sqrt(x,range = None):
-            if range is None:
-                range = [0,x]
+        value = x
 
-            mid = (range[1] + range[0])/2
-            value = mid*mid
+        func = lambda a: a*a - x
 
-            if range[1]-range[0]<0.00001:
-                return int(range[1])
+        while True:
+            next_val = value - (func(value)/(2*value))
+            if next_val//1 == value // 1:
+                return int(next_val//1)
+            else:
+                value = next_val
 
-            mid = (range[1] + range[0])/2
-            value = mid*mid
-
-            if value == x:
-                return int(mid)
-
-            if value > x:
-                range = [range[0],mid]
-
-            if value < x:
-                range = [mid,range[1]]
-
-            return Sqrt(x,range=range)
-
-        return Sqrt(x)
