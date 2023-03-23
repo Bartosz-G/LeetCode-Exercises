35208fe81c2c1322510d6882666f9c789f8fa9f9
Bartosz Gawin <bartex.gw@gmail.com>
2023-03-23 16:13:34 +0000

    SearchInRotatedArray.py Done

---
+++ SearchInRotatedArray.py
@@ -0,0 +1,31 @@
+#https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+
+num1 = [4,5,6,7,0,1,2]
+t1 = 0
+
+num2 = [4,5,6,7,0,1,2]
+t2 = 3
+
+num3 = [1]
+t3 = 0
+
+nums4 = [2,4,5,6,7,0,1]
+t4 = 0
+
+nums5 = [0,1,2,4,5,6,7]
+t5 = 0
+
+
+
+class Solution:
+    def search(self, nums: list[int], target: int,start=None,end=None) -> int:
+
+        length = len(nums) -1
+
+
+
+
+
+
+
